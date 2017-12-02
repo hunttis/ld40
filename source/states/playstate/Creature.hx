5170c933044627193bec32bd9106df13cc7a94c1
Ville Nuutinen <ville.nuutinen@wunderdog.fi>
2017-12-02 11:32:43 +0200

    Simple creature behavior

--- source/states/playstate/Creature.hx
+++ source/states/playstate/Creature.hx
@@ -1,10 +1,12 @@
 package states.playstate;
 
 
+import flixel.math.FlxVelocity;
+import flixel.math.FlxMath;
+import flixel.util.FlxColor;
 import flixel.FlxSprite;
 import flixel.group.FlxGroup;
 import flixel.FlxObject;
-import flixel.util.FlxColor;
 import flixel.FlxG;
 import flixel.addons.display.FlxNestedSprite;
 
@@ -25,15 +27,50 @@ class Creature extends FlxSprite {
     hunger += elapsed;
     // trace(hunger);
 
-    checkForFood();
+    velocity.x = 0;
+    velocity.y = 0;
+
+    if (hunger < 2) {
+      makeGraphic(16, 16, FlxColor.CYAN);
+    }
+
+    if (hunger > 2) {
+      makeGraphic(16, 16, FlxColor.YELLOW);
+
+      if (targetFood == null) {
+        checkForFood();
+      } else {
+        FlxVelocity.moveTowardsObject(this, targetFood, 50);
+      }
+      
+      if (targetFood != null) {
+        FlxG.overlap(this, targetFood, function(self: Creature, food: Food) {
+          food.hurt(0.1);
+          hunger = 0;
+        });
+      }
+    }
 
     if (hunger > 10) {
       makeGraphic(16, 16, FlxColor.RED);
     }
+
   }
 
   private function checkForFood(): Void {
-    // foods.forEachAlive()
+    var closestFood: Food = null;
+    var distance: Float = 1000000;
+    foods.forEachAlive(function(food) {
+      var distanceToFood: Float = FlxMath.distanceBetween(this, food);
+      if (distanceToFood < 100) {
+        if (closestFood == null || distanceToFood < distance) {
+          trace("Found closest food!");
+          closestFood = food;
+          distance = FlxMath.distanceBetween(this, food);
+        }
+      }
+    });
+    targetFood = closestFood;
   }
 
 
