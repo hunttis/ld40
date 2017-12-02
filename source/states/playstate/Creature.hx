ccc21c4d92ceaba7f91cdf5cf93ab6dd913b3671
Ville Nuutinen <ville.nuutinen@wunderdog.fi>
2017-12-02 10:58:00 +0200

    Float initialization

--- source/states/playstate/Creature.hx
+++ source/states/playstate/Creature.hx
@@ -10,17 +10,20 @@ import flixel.addons.display.FlxNestedSprite;
 
 class Creature extends FlxSprite {
 
-  private var hunger: Float;
+  private var hunger: Float = 0;
+  private var targetFood: Food;
+  private var foods: FlxTypedGroup<Food>;
 
   public function new(xLoc: Float, yLoc: Float, foods: FlxTypedGroup<Food>) {
     super(xLoc, yLoc);
     makeGraphic(16, 16, FlxColor.CYAN);
+    this.foods = foods;
   }
 
   override public function update(elapsed: Float) {
     super.update(elapsed);
     hunger += elapsed;
-    trace(hunger);
+    // trace(hunger);
 
     checkForFood();
 
@@ -30,7 +33,7 @@ class Creature extends FlxSprite {
   }
 
   private function checkForFood(): Void {
-
+    // foods.forEachAlive()
   }
 
 
