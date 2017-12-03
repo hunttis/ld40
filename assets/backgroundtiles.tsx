1d93670673bf1c78e97085a0917226541d82f39d
Joni Katajamäki <joni.katajamaki@wunderdog.fi>
2017-12-03 09:10:53 +0200

    Remove duplicate tileset

--- assets/backgroundtiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="backgroundtiles" tilewidth="32" tileheight="32" tilecount="16" columns="8">
- <image source="backgroundtiles.png" width="128" height="128"/>
-</tileset>
--- assets/foregroundtiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="foregroundtiles" tilewidth="32" tileheight="32" tilecount="64" columns="8">
- <image source="foregroundtiles.png" width="128" height="128"/>
-</tileset>
