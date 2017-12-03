hunttis/ld40

commit c8bb660633ac769fe83fb626bef52a6167bf4f77

Add seed machine

source/states/playstate/SeedMachine.hx
@@ -1,0 +1,21 @@
+package states.playstate;
+
+class SeedMachine {
+  var processTime: Float;
+  var gameLevel: GameLevel;
+
+  public function new(gameLevel: GameLevel) {
+    this.gameLevel = gameLevel;
+  }
+
+  public function update(elapsed: Float) {
+    processTime += elapsed;
+
+    if (processTime > 5) {
+      processTime = 0;
+      var x = 200 + (Math.random() * 100);
+      var y = 200 + (Math.random() * 100);
+      gameLevel.items.seeds.add(new Seed(x, y));
+    }
+  }
+}

source/states/playstate/GameLevel.hx
@@ -29,6 +29,7 @@ class GameLevel extends FlxGroup {
   public var salesShip: SalesShip;
   public var importShip: ImportShip;
   public var creatures: FlxTypedGroup<Creature>;
+  public var seedMachine: SeedMachine;
 
   public var shipIndicators: ShipIndicators;
 
@@ -41,6 +42,7 @@ class GameLevel extends FlxGroup {
     checkControls(elapsed);
     checkCollisions(elapsed);
     grass.update(elapsed);
+    seedMachine.update(elapsed);
     creatures.sort(FlxSort.byY);
 		super.update(elapsed);
 	}
@@ -72,9 +74,7 @@ class GameLevel extends FlxGroup {
     var food: Food = createFood(100, 100);
     items.foods.add(food);
 
-    var seed: Seed = createSeed(150, 150);
-    items.seeds.add(seed);
-    
+    seedMachine = new SeedMachine(this);
     
     var weapon: Weapon = createWeapon(200, 200, creatures);
     items.weapons.add(weapon);
@@ -136,11 +136,6 @@ class GameLevel extends FlxGroup {
   private function createFood(x: Float, y: Float): Food {
     var food = new Food(x, y, this);
     return food;
-  }
-
-  private function createSeed(x: Float, y: Float): Seed {
-    var seed = new Seed(x, y, this);
-    return seed;
   }
 
   private function createWeapon(x: Float, y: Float, creatures: FlxTypedGroup<Creature>): Weapon {

source/states/playstate/Seed.hx
@@ -2,7 +2,7 @@ package states.playstate;
 
 class Seed extends Item {
 
-  public function new(xLoc: Float, yLoc: Float, gameLevel: GameLevel) {
+  public function new(xLoc: Float, yLoc: Float) {
     super(xLoc, yLoc, gameLevel);
     type = ItemType.SEED;
     loadGraphic("assets/food.png");
@@ -11,5 +11,4 @@ class Seed extends Item {
   override public function update(elapsed: Float): Void {
     super.update(elapsed);
   }
-
 }

commit f53a22a7dd04ae6b6e4705bf0f594ead436678e3

Fix a bug introduced by commit c8bb660633ac769fe83fb626bef52a6167bf4f77.

source/states/playstate/SeedMachine.hx
@@ -1,7 +1,7 @@
 package states.playstate;
 
 class SeedMachine {
-  var processTime: Float;
+  var processTime: Float = 0.0;
   var gameLevel: GameLevel;
 
   public function new(gameLevel: GameLevel) {