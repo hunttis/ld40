ff2354c7a5dfa9ec10e9c4b8a87627b77e9cae0b
Joni Katajamäki <joni.katajamaki@wunderdog.fi>
2017-12-03 10:51:31 +0200

    Add methods for finding and eating grass

--- source/states/playstate/grass/Grass.hx
+++ source/states/playstate/grass/Grass.hx
@@ -1,6 +1,8 @@
 package states.playstate.grass;
 
 import flixel.tile.FlxTilemap;
+import flixel.math.FlxPoint;
+import flixel.math.FlxMath;
 
 class Grass {
 
@@ -10,6 +12,8 @@ class Grass {
 
   static var GRASS_GROW_DELAY_SECONDS = 0.5;
   public var grassDelay = GRASS_GROW_DELAY_SECONDS;
+
+  var eatableGrass = new Array<FlxPoint>();
 
   public function new(gameLevel: GameLevel) {
     this.gameLevel = gameLevel;
@@ -52,17 +56,13 @@ class Grass {
     }
   }
 
-  function isFullyGrownGrassTile(tile: Int): Bool {
-    return tile == 11;
-  }
+  function isFullyGrownGrassTile(tile: Int): Bool return tile == 11;
 
-  function isGrowingGrassTile(tile: Int): Bool {
-    return tile >= 7 && tile <= 11;
-  }
+  function isGrowingGrassTile(tile: Int): Bool return tile >= 7 && tile <= 11;
 
-  function isBareGroundTile(tile: Int): Bool {
-    return tile == 2;
-  }
+  function isBareGroundTile(tile: Int): Bool return tile == 2;
+
+  function isEatableTile(tile: Int): Bool return tile >= 9 && tile <= 11;
 
   function spreadGrassTo(x: Int, y: Int): Void {
     var tile = grassLayer.getTile(x, y);
@@ -72,7 +72,60 @@ class Grass {
   }
 
   function growGrass(x: Int, y: Int, tile: Int): Void {
-    grassLayer.setTile(x, y, tile + 1);
+    var newTile = tile + 1;
+    grassLayer.setTile(x, y, newTile);
+    if (isEatableTile(newTile)) {
+      eatableGrass.push(new FlxPoint(x, y));
+    }
+  }
+
+  // Returns how fulfilling the grass was.
+  public function eatGrass(x: Int, y: Int): Float {
+    var tile = grassLayer.getTile(x, y);
+    if (!isEatableTile(tile)) {
+      return 0.0;
+    }
+    grassLayer.setTile(x, y, 2);
+    removeEatableGrass(x, y);
+    return switch (tile) {
+      case 9: 0.5;
+      case 10: 1.0;
+      case 11: 1.5;
+      case _: 0.0;
+    }
+  }
+
+  function removeEatableGrass(x: Int, y: Int) {
+    for (i in 0...eatableGrass.length) {
+      var c = eatableGrass[i];
+      if (Std.int(c.x) == x && Std.int(c.y) == y) {
+        eatableGrass.splice(i, 1);
+        return;
+      }
+    } 
+  }
+
+  public function findNearEatableGrass(x: Int, y: Int): FlxPoint {
+    var minDist = FlxMath.MAX_VALUE_INT;
+    var point: FlxPoint = null;
+    for (c in eatableGrass) {
+      var cx = Std.int(c.x);
+      var cy = Std.int(c.x);
+      var dist = manhattanDistance(cx, cy, x, y);
+      if (dist <= 3) {
+        return grassLayer.getTileCoordsByIndex(cx * cy);
+      } else if (dist < minDist) {
+        minDist = dist;
+        point = c;
+      }
+    }
+    return point;
+  }
+
+  public function manhattanDistance(ax: Int, ay: Int, bx: Int, by: Int): Int {
+    var x = if (ax > bx) ax - bx else bx - ax;
+    var y = if (ay > by) ay - by else by - ay;
+    return x + y;
   }
 
   function get_grassLayer() return gameLevel.levelMap.grassLayer;
