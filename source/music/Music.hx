4f357b84dccd9d416faeb0413e213b9bd644d9b7
Ville Peurala <ville.peurala@gmail.com>
2017-12-03 17:27:34 +0200

    Load and cache the background music.

--- source/music/Music.hx
+++ source/music/Music.hx
@@ -1,10 +1,33 @@
 package music;
 
+import flixel.system.FlxSound;
 import flixel.FlxG;
 import flixel.system.FlxAssets.FlxSoundAsset;
+import flixel.system.FlxSound;
+import flixel.system.FlxSoundGroup;
+import flixel.system.frontEnds.SoundFrontEnd;
 
 @:build(flixel.system.FlxAssets.buildFileReferences("assets/music"))
 class Music {
+  var soundFrontEnd: SoundFrontEnd;
+  var backgroundMusicGroup: FlxSoundGroup;
+  var happyTheme: FlxSound;
+
+  public function new() {
+    soundFrontEnd = FlxG.sound;
+    backgroundMusicGroup = new FlxSoundGroup();
+    happyTheme = soundFrontEnd.load(
+      iloinen_teema_v1__ogg,
+      1,
+      true,
+      backgroundMusicGroup,
+      false,
+      false
+    );
+    soundFrontEnd.cache(iloinen_teema_v1__ogg);
+
+  }
+
   @:access(flixel.system.frontEnds.SoundFrontEnd)
   @:access(flixel.system.frontEnds.SoundFrontEnd.music)
   static public function startMusic():Void {
@@ -12,11 +35,9 @@ class Music {
     trace("sound: " + sound);
     trace("Music: " + Music);
 
-    // iloinen_teema_v1: FlxSoundAsset =
-
     // don't restart the music if it's already playing
     if (FlxG.sound.music == null) {
-      FlxG.sound.playMusic("assets/music/iloinen_teema_v1.ogg", 1, true);
+      FlxG.sound.playMusic(iloinen_teema_v1__ogg, 1, true);
     }
   }
 }
