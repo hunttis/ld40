7c7e36b53b9c6c1ba95689ee2805ed200b70f6e0
Ville Peurala <ville.peurala@gmail.com>
2017-12-03 17:03:08 +0200

    Move music-related stuff from Main to Music class.

--- source/Main.hx
+++ source/Main.hx
@@ -1,8 +1,6 @@
 package;
 
-import flixel.FlxG;
 import flixel.FlxGame;
-import flixel.system.FlxAssets.FlxSoundAsset;
 import music.Music;
 import openfl.display.Sprite;
 import states.PlayState;
@@ -12,21 +10,6 @@ class Main extends Sprite {
   public function new() {
     super();
     addChild(new FlxGame(800, 480, PlayState, 1, 60, 60, true));
-    startMusic();
-  }
-
-  @:access(flixel.system.frontEnds.SoundFrontEnd)
-  @:access(flixel.system.frontEnds.SoundFrontEnd.music)
-  static public function startMusic():Void {
-    var sound = FlxG.sound;
-    trace("sound: " + sound);
-    trace("Music: " + Music);
-
-    // iloinen_teema_v1: FlxSoundAsset =
-
-    // don't restart the music if it's already playing
-    if (FlxG.sound.music == null) {
-      FlxG.sound.playMusic("assets/music/iloinen_teema_v1.ogg", 1, true);
-    }
+    Music.startMusic();
   }
 }
--- source/music/Music.hx
+++ source/music/Music.hx
@@ -1,6 +1,22 @@
 package music;
 
+import flixel.FlxG;
 import flixel.system.FlxAssets.FlxSoundAsset;
 
 @:build(flixel.system.FlxAssets.buildFileReferences("assets/music"))
-class Music {}
+class Music {
+  @:access(flixel.system.frontEnds.SoundFrontEnd)
+  @:access(flixel.system.frontEnds.SoundFrontEnd.music)
+  static public function startMusic():Void {
+    var sound = FlxG.sound;
+    trace("sound: " + sound);
+    trace("Music: " + Music);
+
+    // iloinen_teema_v1: FlxSoundAsset =
+
+    // don't restart the music if it's already playing
+    if (FlxG.sound.music == null) {
+      FlxG.sound.playMusic("assets/music/iloinen_teema_v1.ogg", 1, true);
+    }
+  }
+}
