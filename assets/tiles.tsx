727c316ff31e69b6b34a5efd77003353b9c8e65d
Ville Nuutinen <ville.nuutinen@wunderdog.fi>
2017-12-03 11:31:36 +0200

    Placeholder graphics, terrain tiles.

--- assets/tiles.tsx
+++ assets/tiles.tsx
@@ -1,4 +1,20 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tiles" tilewidth="32" tileheight="32" tilecount="16" columns="8">
- <image source="tiles.png" width="128" height="128"/>
+<tileset name="tiles" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="tiles.png" width="256" height="256"/>
+ <terraintypes>
+  <terrain name="asteroid" tile="0"/>
+ </terraintypes>
+ <tile id="16" terrain=",,,0"/>
+ <tile id="17" terrain=",,0,0"/>
+ <tile id="18" terrain=",,0,"/>
+ <tile id="19" terrain="0,0,0,"/>
+ <tile id="20" terrain="0,0,,0"/>
+ <tile id="24" terrain=",0,,0"/>
+ <tile id="25" terrain="0,0,0,0"/>
+ <tile id="26" terrain="0,,0,"/>
+ <tile id="27" terrain="0,,0,0"/>
+ <tile id="28" terrain=",0,0,0"/>
+ <tile id="32" terrain=",0,,"/>
+ <tile id="33" terrain="0,0,,"/>
+ <tile id="34" terrain="0,,,"/>
 </tileset>
