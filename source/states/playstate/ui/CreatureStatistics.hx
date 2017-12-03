d0f4cfd879efc7dd1971305d989f8b60b27871a3
Ville Peurala <ville.peurala@gmail.com>
2017-12-04 00:19:08 +0200

    Shows something about creatures, but not the correct amount yet.

--- source/states/playstate/ui/CreatureStatistics.hx
+++ source/states/playstate/ui/CreatureStatistics.hx
@@ -4,16 +4,16 @@ import flixel.text.FlxText;
 import flixel.group.FlxGroup;
 
 class CreatureStatistics extends FlxGroup {
-  var totalAmountOfCreaturesText:FlxText;
+  var totalAmountOfCreaturesText: FlxText;
 
-  public function new(gameLevel:GameLevel) {
+  public function new(gameLevel: GameLevel) {
     super();
-    totalAmountOfCreaturesText = createText("Total amount of creatures");
+    totalAmountOfCreaturesText = createText("Total amount of creatures", gameLevel.creatures);
     totalAmountOfCreaturesText.scrollFactor.set(0, 0);
     add(totalAmountOfCreaturesText);
   }
 
-  private function createText(name:String): FlxText {
-    return new FlxText(0, 32, 50, "Sook kook", 12, true);
+  private function createText(name: String, creatures: FlxTypedGroup<Creature>): FlxText {
+    return new FlxText(0, 32, 50, 'Sook kook: ${creatures.members.length}', 12, true);
   }
 }
