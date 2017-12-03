36fa28c71fec2335e29199fbeeabab64a42fc454
Ville Peurala <ville.peurala@gmail.com>
2017-12-03 22:39:21 +0200

    Refactoring. Extract method getMusicAsset(filename: String): FlxSoundAsset.

--- source/music/Music.hx
+++ source/music/Music.hx
@@ -64,20 +64,20 @@ class Music {
     currentlyPlaying = theme;
   }
 
-  private static function getHappyThemeAsset(): FlxSoundAsset {
+  private static function getMusicAsset(filename: String): FlxSoundAsset {
     #if flash
-      return "assets/music/happy_theme_v1.mp3";
+      return 'assets/music/$filename.mp3';
     #else
-      return "assets/music/happy_theme_v1.ogg";
+      return 'assets/music/$filename.ogg';
     #end
   }
 
+  private static function getHappyThemeAsset(): FlxSoundAsset {
+    return getMusicAsset("happy_theme_v1");
+  }
+
   private static function getAngryThemeAsset(): FlxSoundAsset {
-    #if flash
-      return "assets/music/angry_theme_v1.mp3";
-    #else
-      return "assets/music/angry_theme_v1.ogg";
-    #end
+    return getMusicAsset("angry_theme_v1");
   }
 
   static public function playHappyTheme(): Void {
