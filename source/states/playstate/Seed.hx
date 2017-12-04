0b9dfa6927f6bcc27d8ab694b9ce217df0cd8417
Ville Nuutinen <hunttis@me.com>
2017-12-04 15:13:25 +0200

    Seeds arrive from space

--- source/states/playstate/Seed.hx
+++ source/states/playstate/Seed.hx
@@ -1,7 +1,10 @@
 package states.playstate;
 
+import flixel.FlxSprite;
+import flixel.tweens.FlxTween;
 class Seed extends Item {
-  public function new(xLoc: Float, yLoc: Float) {
+
+  public function new(xLoc: Float, yLoc: Float, gameLevel: GameLevel) {
     super(xLoc, yLoc, gameLevel);
     type = ItemType.SEED;
     loadGraphic("assets/seed.png");
@@ -9,5 +12,15 @@ class Seed extends Item {
 
   override public function update(elapsed: Float): Void {
     super.update(elapsed);
+  }
+
+  public function landOnAsteroid(tween: FlxTween) {
+    for (i in 1...10){
+      var plop = new FlxSprite(getGraphicMidpoint().x - 8, getGraphicMidpoint().y - 8, "assets/plop.png");
+      // plop.loadGraphic();
+      plop.velocity.x = Math.random() * 200 - 100;
+      plop.velocity.y = Math.random() * 200 - 100;
+      gameLevel.temporaryLayer.add(plop);
+    }
   }
 }
