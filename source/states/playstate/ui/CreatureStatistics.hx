c87e5915bf25855bc7c241f80beb359a9d6c3e98
Ville Nuutinen <ville.nuutinen@wunderdog.fi>
2017-12-04 07:37:54 +0200

    Added update to text information. Only update text when there is a change.

--- source/states/playstate/ui/CreatureStatistics.hx
+++ source/states/playstate/ui/CreatureStatistics.hx
@@ -1,19 +1,56 @@
 package states.playstate.ui;
 
+import states.playstate.creature.BehaviorType;
 import flixel.text.FlxText;
 import flixel.group.FlxGroup;
 
 class CreatureStatistics extends FlxGroup {
-  var totalAmountOfCreaturesText: FlxText;
+
+  var gameLevel: GameLevel;
+
+  var totalAmountOfCreatures: FlxText;
+  var enragedCreatures: FlxText;
 
   public function new(gameLevel: GameLevel) {
     super();
-    totalAmountOfCreaturesText = createText("Total amount of creatures", gameLevel.creatures);
-    totalAmountOfCreaturesText.scrollFactor.set(0, 0);
-    add(totalAmountOfCreaturesText);
+
+    this.gameLevel = gameLevel;
+
+    totalAmountOfCreatures = createText("Creature total", gameLevel.creatures, 32);
+    totalAmountOfCreatures.scrollFactor.set(0, 0);
+
+    enragedCreatures = createText("Enraged", gameLevel.creatures, 48);
+    enragedCreatures.scrollFactor.set(0, 0);
+
+    add(totalAmountOfCreatures);
+    add(enragedCreatures);
   }
 
-  private function createText(name: String, creatures: FlxTypedGroup<Creature>): FlxText {
-    return new FlxText(0, 32, 50, 'Sook kook: ${creatures.members.length}', 12, true);
+  private function createText(name: String, creatures: FlxTypedGroup<Creature>, yLoc): FlxText {
+    return new FlxText(0, yLoc, 0, '${name}: ${creatures.members.length}', 12, true);
+  }
+
+  override public function update(elapsed: Float): Void {
+    super.update(elapsed);
+
+    updateIfNewText(totalAmountOfCreatures, "Creature total: " + gameLevel.creatures.countLiving);
+    updateIfNewText(enragedCreatures, "Enraged: " + getEnragedCreatureCount());
+  }
+
+  private function updateIfNewText(textObject: FlxText, newText: String) {
+    if (textObject.text != newText) {
+      trace("Updating text: " + newText);
+      textObject.text = newText;
+    }
+  }
+
+  private function getEnragedCreatureCount(): Int {
+    var angryCount: Int = 0;
+    for (creature in gameLevel.creatures.members) {
+      if (creature.alive && creature.behavior.getType() == BehaviorType.ANGRY) {
+        angryCount ++;
+      }
+    }
+    return angryCount;
   }
 }
